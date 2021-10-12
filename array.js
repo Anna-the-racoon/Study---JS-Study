45056c8b862c7dace148d84cbd524b556afb0d62
wwww.anechka@gmail.com <wwww.anechka@gmail.com>
2021-10-12 11:12:22 +0300

    Add string and array

---
+++ array.js
@@ -0,0 +1,5 @@
+let fruits = [" Яблоко", " Апельсин", " Слива", ];
+alert( fruits );
+
+let arr = [ 'Яблоко', { name: 'Джон' }, true, function() { alert('привет'); } ];
+alert(arr);
---
+++ string.js
@@ -0,0 +1,14 @@
+let string = `кудах
+-
+тах
+-
+тах`;
+alert(string);
+
+string = "ку\nка\nре\nку";
+alert(string);
+
+alert(typeof(string));
+string = 15;
+alert(typeof(string));
+alert(typeof(String(string)));
